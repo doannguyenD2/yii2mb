ba8cf04b2b7ded34383a08fe3d1c88628164f83e
doannguyenD2 <40143494+doannguyenD2@users.noreply.github.com>
2019-01-03 22:38:30 +0700

    I dont understand why?????/

---
+++ frontend/views/Customer/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\Customer */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="customer-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'email') ?>
+
+    <?= $form->field($model, 'address') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
